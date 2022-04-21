224c875a7d2c0d563793f682bb2057480a473281
Valeria <Koroleva.Valeria.97@yandex.ru>
2022-04-21 19:38:48 +0300

    add 3 task and 2

--- src/features/restaurant/ui/restaurants/component.jsx
+++ src/features/restaurant/ui/restaurants/component.jsx
@@ -3,14 +3,14 @@ import { Restaurant } from "../restaurant/component";
 
 
 export const Restaurants = ({ restaurants }) => {
-  let [count, setCount] = useState(0);
+  let [restaurantIndex, setCount] = useState(0);
   // Полняла,ч то можно применить count in <Restaurant restaurant={restaurants[count]} />
   // Но вопрос, как и где применить setCount. Если он здест нужен.
   return (
     <div>
       <div>{restaurants.map((restaurant) => (
-      <button key={restaurant.id}> {restaurant.name} </button>))}</div>
-      <Restaurant restaurant={restaurants[count]} />
+      <button key={restaurant.id} onClick={() => setCount(restaurantIndex)}> {restaurant.name} </button>))}</div>
+      <Restaurant restaurant={restaurants[restaurantIndex]} />
     </div>
   );
 };
--- src/features/review/ui/new-review/new-review.jsx
+++ src/features/review/ui/new-review/new-review.jsx
@@ -3,9 +3,11 @@ import { useReducer } from "react";
 const reducer = (state, action) => {
   switch (action.type) {
     case "changeName":
-      return { name: action.payload, text: "" };
+      return { name: action.payload, text: "", rating: "" };
     case "changeText":
       return { ...state, text: action.payload };
+      case "changeRating":
+      return { ...state, rating: action.payload };
     default:
       return state;
   }
@@ -15,6 +17,7 @@ export const NewReview = () => {
   const [state, dispatch] = useReducer(reducer, {
     name: "DefaultName",
     text: "text",
+    rating: "",
   });
 
   return (
@@ -34,6 +37,16 @@ export const NewReview = () => {
           value={state.text}
           onChange={(event) => {
             dispatch({ type: "changeText", payload: event.target.value });
+          }}
+        />
+      </div>
+      <div>
+        <span>Rating</span>
+        <input
+          value={state.rating}
+          type="number"
+          onChange={(event) => {
+            dispatch({ type: "changeRating", payload: event.target.value });
           }}
         />
       </div>
